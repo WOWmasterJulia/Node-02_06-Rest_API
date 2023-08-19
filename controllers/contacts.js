2838cfaac4a4004ed30d752d8a2c9fa744f26f17
WOWmasterJulia <topazzz@ukr.net>
2023-08-19 23:58:15 +0300

    changes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -46,6 +46,9 @@ const removeContact = async (req, res) => {
 const updateFavorite = async (req, res) => {
   const { id } = req.params;
   const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  // if (!Object.keys(req.body).includes("favorite")) {
+  //   throw HttpError(400, "missing field favorite");
+  // }
   if (!result) {
     throw HttpError(404, "Not found");
   }

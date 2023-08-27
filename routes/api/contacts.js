2c98c5f99b6ef4927d4df9476e7d88fcb04d5f55
WOWmasterJulia <topazzz@ukr.net>
2023-08-27 20:31:29 +0300

    All Changes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,37 @@
 const express = require("express");
 
-
-
 // const ctrl = require("../../controllers/contacts_all");
 const ctrl = require("../../controllers/index.js");
 
 
-
-
 // const schemas = require("../../schemas/contacts");
 const { schemas } = require("../../models/contact");
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 const router = express.Router();
 
 // маршруты:
-router.get("/contacts", ctrl.listContacts);
-router.get("/contacts/:id", isValidId, ctrl.getById);
-router.post("/contacts", validateBody(schemas.addSchema), ctrl.addContact);
-router.put("/contacts/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateContact);
-router.patch("/contacts/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
-router.delete("/contacts/:id", isValidId, ctrl.removeContact);
+router.get("/contacts", authenticate, ctrl.listContacts);
+router.get("/contacts/:id", authenticate, isValidId, ctrl.getById);
+router.post(
+  "/contacts",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrl.addContact
+);
+router.put(
+  "/contacts/:id",
+  authenticate,
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateContact
+);
+router.patch(
+  "/contacts/:id/favorite",
+  authenticate,
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateFavorite
+);
+router.delete("/contacts/:id", authenticate, isValidId, ctrl.removeContact);
 
 module.exports = router;

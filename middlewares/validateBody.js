501bff5483508235c9acf42bdcdf981988d357a1
WOWmasterJulia <topazzz@ukr.net>
2023-08-28 01:23:44 +0300

    changes

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,18 +1,28 @@
 const { HttpError } = require("../helpers");
 
+// const validateBody = (schema) => {
+//   const func = (req, res, next) => {
+//     console.log(req.body)
+//     const { error } = schema.validate(req.body);
+//     const empty = req._body;
+
+//     if (!empty) {
+//       next(HttpError(400, "missing fields"));
+//     }
+//     if (error) {
+//       next(HttpError(400, error.message));
+//     }
+//     next();
+//   };
+//   return func;
+// };
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
-    const empty = req._body;
-
-    if (!empty) {
-      next(HttpError(400, "missing fields"));
-    }
-    if (error) {
-      next(HttpError(400, error.message));
-    }
+    if (error) next(HttpError(400, error.message));
     next();
   };
+
   return func;
 };
 

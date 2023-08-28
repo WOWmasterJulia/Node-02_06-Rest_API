a2caf49d74336d3d448bea07778a1346fd1e6000
WOWmasterJulia <topazzz@ukr.net>
2023-08-28 15:31:57 +0300

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,22 +7,26 @@ const { HttpError } = require("../helpers");
 const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
-  const { authenticate = "" } = req.headers;
-  const [bearer, token] = authenticate.split(" ");
+  // const { authenticate = "" } = req.headers;
+  // const [bearer, token] = authorization.split(" ");
+  const [bearer, token] = req.headers.authorization.split(" ");
   console.log(token);
   
-  console.log(req.user);
+  
   if (bearer !== "Bearer") {
     next(HttpError(401, "Not authorized"));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    if (!user || !user.token || user.token !== token) {
+    // if (!user || !user.token || user.token !== token) {
+    if (!user) {
       next(HttpError(401, "No authorized"));
     }
     req.user = user; // запис хто робить запит
-    console.log(user);
+    // console.log(user);
+    // console.log(req.user);
+
     next();
   } catch {
     next(HttpError(401, "No authorize"));

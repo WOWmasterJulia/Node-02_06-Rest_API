046e7892205f335403f74b90f8c24565eb78a60d
WOWmasterJulia <topazzz@ukr.net>
2023-08-30 00:47:39 +0300

    Changes

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,20 +7,20 @@ const { HttpError } = require("../helpers");
 const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
-  // const { authenticate = "" } = req.headers;
-  // const [bearer, token] = authorization.split(" ");
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
   // console.log(req.headers.authorization);
-  if (!req.headers.authorization) {
-    next(HttpError(401, "Not authorization"));
-    return
-  }
-  const [bearer, token] = req.headers.authorization.split(" ");
-  console.log(token);
+  // if (!req.headers.authorization) {
+  //   next(HttpError(401, "Not authorization"));
+  //   return;
+  // }
+  // const [bearer, token] = req.headers.authorization.split(" ");
+  // console.log(token);
 
-  if (bearer !== "Bearer" || !token) {
+  if (bearer !== "Bearer") {
     next(HttpError(401, "Not authorized"));
   }
-  
+  // || !token
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     // console.log(id);
@@ -31,7 +31,6 @@ const authenticate = async (req, res, next) => {
       next(HttpError(401, "No authorized"));
     }
     req.user = user; // запис хто робить запит
-
 
     next();
   } catch {
